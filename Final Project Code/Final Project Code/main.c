45cdadfd38c1ebe88663b75ad1bbca53e46d1584
Jessi Jha <jessijha@seas.upenn.edu>
2024-04-26 02:15:51 -0400

    starting to combine code

---
+++ Final Project Code/Final Project Code/main.c
@@ -0,0 +1,18 @@
+/*
+ * Sensors_Atmega_ESP.c
+ *
+ * Created: 4/26/2024 2:11:54 AM
+ * Author : jessi
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
